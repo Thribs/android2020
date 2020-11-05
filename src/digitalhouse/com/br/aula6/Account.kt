eaabf9304d87fca9fe6466906711497397414061
= <thiago@clubedochaveiro.com.br>
2020-11-05 17:39:39 -0300

    Before deleting 'out' folder

--- src/digitalhouse/com/br/aula6/Account.kt
+++ src/digitalhouse/com/br/aula6/Account.kt
@@ -1,6 +1,9 @@
 package digitalhouse.com.br.aula6
 
 class Account(val accNum: Int, var balance: Double, owner: Client) {
+    init {
+        println("Conta de número $accNum, pertencente a ${owner.name} ${owner.surname}.\nSaldo da conta: $balance")
+    }
     fun deposit(amount: Double) : String{
         balance += amount
         return "Realizada a operação de depósito.\nNovo saldo: $balance"
--- src/digitalhouse/com/br/aula6/Exercicio1.kt
+++ src/digitalhouse/com/br/aula6/Exercicio1.kt
@@ -1,7 +1,10 @@
 package digitalhouse.com.br.aula6
 
 fun main(){
+    println("Conta: ")
     val client1 = Client("Thiago", "Ribeiro")
     val client2 = Client("Rafael", "Silva")
-    println(client1)
+    var account1 = Account(1, 0.0, client1)
+    print(account1)
+    var account2 = Account(2, 100.0, client2)
 }
