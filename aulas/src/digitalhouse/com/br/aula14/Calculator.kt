11c78d178b15eca111ab1bbbd8d03b2abbc08ae2
Thribs <thribsilva@gmail.com>
2020-11-23 21:40:13 -0300

    Aula 14

---
+++ aulas/src/digitalhouse/com/br/aula14/Calculator.kt
@@ -0,0 +1,12 @@
+package digitalhouse.com.br.aula14
+
+/*
+Crie uma calculadora:
+Sua calculadora deve ser capaz de realizar operações matemáticas recebendo 2 parâmetros de entrada e retornando o resultado da operação.
+Implemente as operações de soma, subtração, multiplicação e divisão.
+Teste sua calculadora na função main (a calculadora deve executar as operacoes).
+Agora na sua função main crie lambda functions para cada uma das operações matemáticas sem a utilização de interfaces.
+Teste sua nova calculadora sem utilizar interfaces.
+ */
+class Calculator {
+}
