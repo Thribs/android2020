6a8b3e95efec9dbc180f16379be6eb672d8d213c
Thribs <thribsilva@gmail.com>
2021-02-08 14:47:09 -0300

    Revisão aula 26

--- aulasandroid/aula26/Fragment/app/src/main/java/com/example/fragment/FragmentExample1.kt
+++ aulasandroid/aula26/Fragment/app/src/main/java/com/example/fragment/FragmentExample1.kt
@@ -6,9 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FragmentExample : Fragment() {
+class FragmentExample1 : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_example, container, false)
+        return inflater.inflate(R.layout.fragment_example1, container, false)
     }
 }
