ca9e866ce8d5e7ee7c321d2a00397443522cda37
ThribsClube <thiago@clubedochaveiro.com.br>
2021-03-22 18:00:37 -0300

    Arch result contract

--- aulasandroid/Arch/app/src/main/java/com/digitalhouse/arch/ui/products/ProductsActivity.kt
+++ aulasandroid/Arch/app/src/main/java/com/digitalhouse/arch/ui/products/ProductsActivity.kt
@@ -1,10 +1,10 @@
 package com.digitalhouse.arch.ui.products
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -17,9 +17,18 @@ import com.digitalhouse.arch.utilities.InjectorUtils
 class ProductsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductsBinding
+
     private lateinit var products: LiveData<List<Product>>
+
+    private lateinit var viewModel: ProductsViewModel
+
+    val newProductResult = registerForActivityResult(NewProductContract()) { product ->
+        viewModel.addProduct(product)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityProductsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -43,14 +52,20 @@ class ProductsActivity : AppCompatActivity() {
         recycler.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
 
         binding.newProductButton.setOnClickListener() {
-            val intent = Intent(this, NewProductActivity::class.java)
-            getActivityForResult.launch(intent)
+
+            newProductResult.launch("test")
         }
     }
-    lateinit var name: String
-    private val getActivityForResult =
-    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val resultIntent = result.data
-            name = resultIntent?.getStringExtra("name") ?: "Product name"
+
+    class NewProductContract : ActivityResultContract<String, Product>() {
+        override fun createIntent(context: Context, input: String?): Intent =
+            Intent(context, NewProductActivity::class.java)
+
+        override fun parseResult(resultCode: Int, intent: Intent?) =
+            Product(
+                0,
+                intent?.getStringExtra("name") ?: "Chickens",
+                intent?.getStringExtra("price")?.toDouble() ?: 0.0
+            )
     }
 }
