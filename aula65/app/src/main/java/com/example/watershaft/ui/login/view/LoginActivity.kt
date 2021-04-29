9b804f6f5fc9dd5a5d9dfa32c57d6ce2d113917a
Thribs <thribsilva@gmail.com>
2021-04-28 22:06:44 -0300

    Aula 65

--- aula65/app/src/main/java/com/example/watershaft/ui/login/view/LoginActivity.kt
+++ aula65/app/src/main/java/com/example/watershaft/ui/login/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.watershaft
+package com.example.watershaft.ui.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.watershaft.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
